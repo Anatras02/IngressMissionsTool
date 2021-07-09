5dd96bb38573a376efb469212e8e0144e5d98ac6
Anatras02 <alessio.balderi@adraise.it>
2021-07-09 20:36:40 +0200

    Increased timeout

--- functions/driver.py
+++ functions/driver.py
@@ -9,7 +9,7 @@ def go_next(driver):
     driver.find_element_by_xpath("//*[contains(@ng-click, 'goNext()')]").click()
 
 
-def wait_for_it(driver, name, by=By.CLASS_NAME, timeout=10):
+def wait_for_it(driver, name, by=By.CLASS_NAME, timeout=60):
     element_present = EC.presence_of_element_located((by, name))
     WebDriverWait(driver, timeout).until(element_present)
 
